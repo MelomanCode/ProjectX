310832a0d261892025aa423bca047a68a206e52e
MelomanCode <dmitryKezik@gmail.com>
2023-09-29 14:44:20 +0200

    v.0.0.0

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,26 +7,33 @@ import { pianoKeys } from '../constances/piano-keys.constance';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  private audio: HTMLAudioElement;
+  private audio: HTMLAudioElement = new Audio();
   private volumeValue = 0.5;
-
-  constructor() {
-    this.audio = new Audio();
-    this.audio.volume = this.volumeValue;
-  }
+  showKeys = true;
 
   keys: { label: string; isBlack: boolean }[] = pianoKeys;
 
   playSound(note: string) {
     const audio = new Audio(`./assets/samples/${note}.mp3`);
+    audio.currentTime = 0.5
     audio.volume = this.volumeValue;
+    this.audio = audio
     audio.play().catch((error) => {
       console.error('ERROR, DESTROY YOUR COMPUTER', error);
     });
   }
 
+  toggleKeysVisibility() {
+    this.showKeys = !this.showKeys
+  }
+
+
   volumeChange(event: any) {
     this.volumeValue = event.target.value
       this.audio.volume = this.volumeValue;
     }
+
+
+
+
 }
